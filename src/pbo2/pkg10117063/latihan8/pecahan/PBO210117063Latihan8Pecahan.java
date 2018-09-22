00a7085fc12d3cb1d3fa4936e9934b5a586e07fa
Cessario <43488314+Cessario@users.noreply.github.com>
2018-09-22 20:29:06 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117063/latihan8/pecahan/PBO210117063Latihan8Pecahan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117063.latihan8.pecahan;
+
+/**
+ *
+ * @author DEADPOOL
+ *NAMA : Cessario
+ *NIM : 10117063
+ *KELAS : IF-2
+ *Deskripsi Program : menampilkan pecahan*/
+public class PBO210117063Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.1234789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
